1f50e4c5c60f0adba2297b7491341262e371d975
Dhruv Bansal <dhruvbansalup@gmail.com>
2025-01-28 19:00:24 +0530

    Done basic app configuration and add database models

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
 
 app=Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('login.html')
+app.config.from_object(Config) # Load the Config object from config.py
 
+db=SQLAlchemy(app)
+
+from models import *
+
+from routes import *
 
 if __name__=='__main__':
     app.run(debug=True)
