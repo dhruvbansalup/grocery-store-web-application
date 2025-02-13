20d3350e14d5bc29048ebd51521a0b655fc72f3b
Dhruv Bansal <dhruvbansalup@gmail.com>
2025-02-14 04:00:14 +0530

    add profile page and add logout functionality

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Grocery Management Application
+
+## Create virtual environment and install dependencies
+```bash
+python3 -m venv venv
+
+# For Windows
+    venv\Scripts\activate
+# For Linux
+    source venv/bin/activate
+
+pip install -r requirements.txt
+```
--- routes.py
+++ routes.py
@@ -3,15 +3,8 @@ from app import app
 from models import User, Category, Product, Cart, Transaction, Order
 from models import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from functools import wraps
 
-@app.route('/')
-def index():
-    #CHECK IF USER IS LOGGED IN
-    if 'user_id' in session:
-        return render_template('index.html')
-    else:
-        flash('Please login to continue')
-        return redirect(url_for('login'))
 
 @app.route('/login')
 def login():
@@ -74,4 +67,68 @@ def register_post():
     db.session.commit()
     
     flash('User registered successfully with username: '+str(username))
+    return redirect(url_for('login'))
+
+# Decorator For Checking if the user is currently login
+def auth_required(func):
+    @wraps(func)
+    def inner(*args,**kwargs):
+        if 'user_id' in session:
+            return func(*args,**kwargs)
+        else:
+            flash('Please login to continue')
+            return redirect(url_for('login'))
+    return inner
+
+@app.route('/')
+@auth_required
+def index():
+    return render_template('index.html')
+
+@app.route('/profile')
+@auth_required
+def profile():
+    user=User.query.get(session['user_id'])
+    return render_template("profile.html",user=user)
+
+@app.route('/profile',methods=['POST'])
+@auth_required
+def profile_post():
+    username=request.form.get("username")
+    name=request.form.get("name")
+    cpassword=request.form.get("cpassword")
+    password=request.form.get("password")
+
+     #VALIDATIONS
+    if not password or not cpassword or not username:
+        flash('Please fill required fields')
+        return redirect(url_for('profile'))
+    
+    user=User.query.get(session['user_id'])
+
+    if not check_password_hash(user.passhash, cpassword):
+        flash('Invalid Current password')
+        return redirect(url_for('profile'))
+
+    if username!=user.username:
+        new_username=User.query.filter_by(username=username).first()
+        if new_username:
+            flash("Username already exists")
+            return redirect(url_for('profile'))
+    
+
+    new_pass_hash=generate_password_hash(password)
+    user.username=username
+    user.passhash=new_pass_hash
+    user.name=name
+    db.session.commit()
+    flash("Profile Updated Successfully!!")
+    return redirect(url_for('profile'))
+
+
+@app.route('/logout')
+@auth_required
+def logout():
+    print(session)
+    session.pop('user_id')
     return redirect(url_for('login'))
